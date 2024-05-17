0b117997c4e3f589c74f9741b620845c31038c87
Santosh Managuli <rocksantosh15@gmail.com>
2024-05-17 08:17:05 +0530

    changes

--- src/components/Records/Records.jsx
+++ src/components/Records/Records.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import styles from './Records.module.css'
+import React, { useState } from "react";
+import styles from "./Records.module.css";
+import Search from "../Search/Search";
 
 export default function Records() {
+  const [search, setSearch] = useState('')
   return (
     <div className={styles.container}>
       <div className={styles.main}>
-        
+        <Search data={search}/>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Search({data}) {
+  console.log(props);
+  return (
+    <div>
+      <input type="search" name='records' value={data} />
+    </div>
+  )
+}

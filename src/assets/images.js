072c1c2999c0ec01b129c5195f2e91bc2283e8ad
Santoshm <santoshm@192.168.1.6>
2024-05-18 13:56:42 +0530

    created a seperate file for importing images to app

---
+++ src/assets/images.js
@@ -0,0 +1,46 @@
+const home = new URL("./home.svg", import.meta.url).href;
+const right_arrow = new URL(".right-arrow.svg", import.meta.url).href;
+const filter = new URL("./filter.svg", import.meta.url).href;
+const logo = new URL("./müellif-logo.svg", import.meta.url).href;
+const dashboard = new URL("./dashboard.svg", import.meta.url).href;
+const collections = new URL("./collections.svg", import.meta.url).href;
+const templates = new URL("./templates.svg", import.meta.url).href;
+const works = new URL("./works.svg", import.meta.url).href;
+const admin = new URL("./admin-panel.svg", import.meta.url).href;
+const reports = new URL("./reports.svg", import.meta.url).href;
+const profileLogo = new URL("./preview.svg", import.meta.url).href;
+const userCard = new URL("./user-card.png", import.meta.url).href;
+const downArrow = new URL("./down-arrow.svg", import.meta.url).href;
+const upArrow = new URL("./up-arrow.svg", import.meta.url).href;
+const unfilled_star = new URL("./star-empty.svg", import.meta.url)
+  .href;
+const star_filled = new URL("./star-filled.svg", import.meta.url)
+  .href;
+const preview = new URL("./preview.svg", import.meta.url).href;
+const copy_url = new URL("./copyurl.svg", import.meta.url).href;
+const info = new URL("./information.svg", import.meta.url).href;
+const continue_template = new URL("./continue.svg", import.meta.url)
+  .href;
+
+export {
+  home,
+  right_arrow,
+  filter,
+  userCard,
+  profileLogo,
+  reports,
+  admin,
+  works,
+  templates,
+  collections,
+  dashboard,
+  logo,
+  downArrow,
+  upArrow,
+  unfilled_star,
+  star_filled,
+  preview,
+  copy_url,
+  info,
+  continue_template,
+};
